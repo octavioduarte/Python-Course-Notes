ddb3858982532bf6f3636be0be561956604fdb80
octavioduarte <octavioduarte2105@gmail.com>
2024-03-29 15:21:38 -0300

    doc: add instructions about if,else and elif

---
+++ 05-if/01-basic-if-with-else.py
@@ -0,0 +1,6 @@
+inputUser = input("Where are you from? ")
+
+if (inputUser == "Brazil"):
+    print("We're from the same country")
+else:
+    print("You're foreigner")
---
+++ 05-if/02-basic-if-with-elif.py
@@ -0,0 +1,11 @@
+inputUser = input("What your favorite language programming [Node, Python or C]? ")
+
+if (inputUser == "Node"):
+    print("Cool, it's a good language to Real-time applications.")
+elif (inputUser == "Python"):
+    print("Cool, it's a good language to AI.")
+elif (inputUser == "C"):
+    print("....")
+    
+    
+# For the annoying people on duty, I know that Node is not a language
