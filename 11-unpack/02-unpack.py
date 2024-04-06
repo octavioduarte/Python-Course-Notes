2add78e23504c5c1d6f2a6c5d86317eb0c800798
octavioduarte <octavioduarte2105@gmail.com>
2024-04-06 14:31:45 -0300

    doc: add instructions about diff ways to unpack

---
+++ 11-unpack/02-unpack.py
@@ -0,0 +1,16 @@
+full_name = ['Sport', 'Club', 'Corinthians', 'Paulista']
+
+
+# The same idea:
+
+# Idea 1:
+slice1, slice2, slice3, slice4 = full_name
+print(slice1, slice2, slice3, slice4)
+
+
+# Idea 2:
+print(*full_name)
+
+
+# Obviously, we don't have the variables, but we extract values from our
+# list equivalent to that on line 8.
