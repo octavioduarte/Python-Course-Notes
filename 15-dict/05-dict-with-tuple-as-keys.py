d98d9f1247f30a486fbb53ef0302bbb288b14ce4
octavioduarte <octavioduarte2105@gmail.com>
2024-04-06 13:01:03 -0300

    doc: add instrutions about dict with tuples as keys

---
+++ 15-dict/05-dict-with-tuple-as-keys.py
@@ -0,0 +1,15 @@
+animals = {
+    ('Aquatic', 'Big'): 'Whale',
+    ('Flier', 'Good Eye'): 'Hawk'
+}
+
+
+print(animals.keys())
+print(animals.values())
+
+"""
+    output
+    
+    dict_keys([('Aquatic', 'Big'), ('Flier', 'Good Eye')])
+    dict_values(['Whale', 'Hawk'])
+"""
